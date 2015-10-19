53267f5e2e5c967d99b3c50b8c1a18147bdeced1
hd85 <hdossa01@gmail.com>
2015-10-19 14:23:21 -0400

    added twitter text gem to show make links in tweets clickable

--- Twimiter/app/helpers/application_helper.rb
+++ Twimiter/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+require 'twitter-text'
 module ApplicationHelper
+  include Twitter::Autolink
+
+  def twitter_text(text)
+    text = auto_link(text)
+    text ? text.html_safe : ''
+  end
 end
--- Twimiter/app/models/limiter.rb
+++ Twimiter/app/models/limiter.rb
@@ -6,27 +6,27 @@ class Limiter
 
 	def limit(user)
 		timeline = user.twitter_client.home_timeline(count: 200)
-		#timeline2 = $twitter.home_timeline(count: 200, max_id: timeline.last.id)
-		#timeline3 = $twitter.home_timeline(count: 200, max_id: timeline2.last.id)
-		#timeline4 = $twitter.home_timeline(count: 200, max_id: timeline3.last.id)
-		#timeline5 = $twitter.home_timeline(count: 200, max_id: timeline4.last.id)
+		timeline2 = user.twitter_client.home_timeline(count: 200, max_id: timeline.last.id)
+		timeline3 = user.twitter_client.home_timeline(count: 200, max_id: timeline2.last.id)
+		timeline4 = user.twitter_client.home_timeline(count: 200, max_id: timeline3.last.id)
+		timeline5 = user.twitter_client.home_timeline(count: 200, max_id: timeline4.last.id)
 
-		#real_timeline = [timeline, timeline2, timeline3, timeline4, timeline5]
+		real_timeline = [timeline, timeline2, timeline3, timeline4, timeline5]
 
 		twinty = {}
-		#real_timeline.each do |timeline|
+		real_timeline.each do |timeline|
 			timeline.each do |tweet|
 		  	user_id = tweet.user
 		  		if twinty[user_id].nil?
 						twinty[user_id] = []
 		  		end
 		  	twinty[user_id] << tweet
-		#	end
+			end
 		end
 
 		twinty_one = []
 		twinty.each do |key, value|
-			tweet_stat = TweetStat.most_recent_for_user(key.id)
+			tweet_stat = TweetStat.find_by(user_id: key.id, for_date: Date.today.to_datetime)
 			if tweet_stat
 				twinty_one << value.select do |tweet|
 		 			tweet.retweet_count > tweet_stat.rt_per_tweet
