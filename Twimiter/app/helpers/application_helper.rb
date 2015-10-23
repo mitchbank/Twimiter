c16442b30abf99c52336b06763a384eb980498ad
hd85 <hdossa01@gmail.com>
2015-10-22 21:04:59 -0400

    added functionality to click on links and open in new tabs

--- Twimiter/app/helpers/application_helper.rb
+++ Twimiter/app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   include Twitter::Autolink
 
   def twitter_text(text)
-    text = auto_link(text)
+    text = auto_link(text, target: "_blank")
     text ? text.html_safe : ''
   end
 end
